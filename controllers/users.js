48a3beff8c83b8f3c0bccb310c1180c41d8fc8a3
Timur Harchenko <support.solomon@yandex.ru>
2023-04-18 08:56:06 +0400

    пр13 обработка ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,25 @@
 const User = require('../models/user');
+const {BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, handleErrors, handleErrorNotFound} = require('../utils/errors');
 
 module.exports.getUserById = (req, res) => {
   const _id = req.params.userId
   User.findById({ _id })
     .then(user => {
-      res.send({ data: user })
+      handleErrorNotFound(user, res, "Пользователь по указанному _id не найден.")
+      res.status(200).send({ data: user })
     })
-    .catch((err) => console.log(err, res));
+    .catch((err) => handleErrors(err, res));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(user => res.send({ data: user }))
-    .catch((err) => console.log(err, res));
+    .then(user => res.status(201).send({ data: user }))
+    .catch((err) => handleErrors(err, res));
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then(users => res.send({ data: users }))
-    .catch((err) => console.log(err, res));
+    .then(users => res.status(200).send({ data: users }))
+    .catch((err) => handleErrors(err, res));
 };
---
+++ utils/errors.js
@@ -0,0 +1,25 @@
+const BAD_REQUEST = 400;
+const INTERNAL_SERVER_ERROR = 500;
+const NOT_FOUND = 404;
+
+function handleErrors (error, response) {
+  if (error instanceof CastError || error instanceof ValidationError) {
+    return response.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+  }
+
+  return response.status(INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${INTERNAL_SERVER_ERROR}` });
+};
+
+function handleErrorNotFound (obj, response, string){
+  if(!obj){
+    return response.status(NOT_FOUND).send({ message: string })
+  }
+};
+
+module.exports = {
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  handleErrors,
+  handleErrorNotFound
+};
