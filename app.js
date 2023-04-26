2d7ec99cf412b8fbede82e59178cfcb24fd7b7c3
Timur Harchenko <support.solomon@yandex.ru>
2023-04-26 17:16:12 +0400

    пр14 joi validate errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 const routes = require('./routes/index');
 
 const { PORT = 3000, BASE_PATH = 'localhost' } = process.env;
@@ -17,6 +18,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
